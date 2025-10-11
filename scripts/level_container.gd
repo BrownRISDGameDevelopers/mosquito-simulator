326c4ea692e6469dce5e90fc02e8e56e0f4f65b5
Brian Xu <xu.brian.s@gmail.com>
2025-10-11 14:20:48 -0400

    minigame transitions

--- scripts/level_container.gd
+++ scripts/level_container.gd
@@ -2,10 +2,23 @@ extends Control
 
 var minigame_visible = false
 
-func _on_map_minigame_toggle():
-	minigame_visible = !minigame_visible
+@onready var map_3d = $MapViewport/SubViewport/Map
+@onready var minigame = $MinigameViewport/SubViewport/SwatMinigame
+
+func toggle_minigame(minigame_state):
+	minigame_visible = minigame_state
 	$MinigameViewport.visible = minigame_visible
 	if minigame_visible:
 		$MapViewport.scale = Vector2(0.25, 0.25)
 	if not minigame_visible:
 		$MapViewport.scale = Vector2.ONE
+
+
+func _on_map_minigame_toggle():
+	toggle_minigame(true)
+	minigame.reset()
+
+
+func _on_swat_minigame_exited_bounds():
+	toggle_minigame(false)
+	map_3d.free_player()
