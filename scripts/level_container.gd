e180cea62a099758d195620295ab67a8ac48144c
liteless <65517797+liteless@users.noreply.github.com>
2025-10-11 21:07:00 -0400

    feat(transitions): create global transition manager

--- scripts/level_container.gd
+++ scripts/level_container.gd
@@ -2,6 +2,17 @@ extends Control
 
 var minigame_visible = false
 
+@onready var map_viewport = $MapViewport/SubViewport
+
+func _ready() -> void:
+	set_level(Global.starting_level)
+
+func set_level(map: PackedScene):
+	var level_instance = map.instantiate()
+	for child in map_viewport.get_children():
+		child.queue_free()
+	map_viewport.add_child(level_instance)
+
 func _on_map_minigame_toggle():
 	minigame_visible = !minigame_visible
 	$MinigameViewport.visible = minigame_visible
