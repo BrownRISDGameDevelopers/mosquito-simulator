3336bb6dc318e360d7a3bbb5b7fef4a04b07e929
jessiiishin <jessiiishin@gmail.com>
2025-11-01 16:33:41 -0400

    menu buttons are working except for replay

--- scripts/end_screen_menu_items.gd
+++ scripts/end_screen_menu_items.gd
@@ -4,7 +4,11 @@ const LEVEL_CONTAINER = preload("res://scenes/LevelContainer.tscn")
 const CREDITS = preload("res://scenes/Credits.tscn")
 const MAIN_MENU = preload("res://scenes/MainMenu.tscn")
 
+@export var label_message = "Insert message in menu"
+@onready var label: Label = $VBoxContainer/Label
 
+func _ready() -> void:
+	label.text = label_message
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_packed(MAIN_MENU)
@@ -12,6 +16,7 @@ func _on_main_menu_button_pressed() -> void:
 
 func _on_replay_button_pressed() -> void:
 	get_tree().change_scene_to_packed(LEVEL_CONTAINER)
+	# replay function needed....
 
 
 func _on_quit_pressed() -> void:
