3347b8d1955f7b1142b4cb47b46074c02dc5edd4
jessiiishin <jessiiishin@gmail.com>
2025-11-01 16:15:56 -0400

    renamed to WinScreen again i decided it's just simpler to separate them

--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -5,6 +5,7 @@ const CREDITS = preload("res://scenes/Credits.tscn")
 const MAIN_MENU = preload("res://scenes/MainMenu.tscn")
 
 
+
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_packed(MAIN_MENU)
 
