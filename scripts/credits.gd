8a551c2118e8fc258e8126f0ab79fcaa43b157f4
liteless <65517797+liteless@users.noreply.github.com>
2025-11-22 16:50:45 -0500

    feat(ui): credits, life blood icon and bite indicator

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -2,3 +2,11 @@ extends Control
 
 func _on_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
+
+func _on_return_mouse_exited() -> void:
+	$Return.material.set_shader_parameter("enabled", false)
+
+
+func _on_return_mouse_entered() -> void:
+	$Return.material.set_shader_parameter("enabled", true)
